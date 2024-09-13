e3be936a9f07d2c2557d66d28561f32ccae01a3c
sasineelakandan <devasasi887@gmail.com>
2024-09-13 10:15:12 +0530

    server Start

---
+++ Server/src/index.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+const port = 3000;
+
+
+app.use(express.json());
+
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, TypeScript!');
+});
+
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
